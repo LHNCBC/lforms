d720e909e2af8a0d12e355e17355b68d401cf71c
Frank Tao <taof@lhcs-franktao-lx.nlm.nih.gov>
2014-12-19 10:23:45 -0500

    bug fixing: ie9 crashing due to undefined window.console (continued)

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -107,15 +107,17 @@ angular.module('lformsWidget')
  * http://stackoverflow.com/questions/3326650/console-is-undefined-error-for-internet-explorer.
  **/
 (function() {
-  if (!window.console) 
+  if (!window.console) {
     window.console = {};
-  // union of Chrome, FF, IE, and Safari console methods
-  var m = [ "log", "info", "warn", "error", "debug", "trace", "dir", "group",
-     "groupCollapsed", "groupEnd", "time", "timeEnd", "profile", "profileEnd",
-     "dirxml", "assert", "count", "markTimeline", "timeStamp", "clear"];
-  // define undefined methods as noops to prevent errors
-  for (var i = 0; i < m.length; i++) {
-    if (!window.console[m[i]]) 
-      window.console[m[i]] = function() {};
-  } 
+    // union of Chrome, FF, IE, and Safari console methods
+    var m = [ "log", "info", "warn", "error", "debug", "trace", "dir", "group",
+      "groupCollapsed", "groupEnd", "time", "timeEnd", "profile", "profileEnd",
+      "dirxml", "assert", "count", "markTimeline", "timeStamp", "clear"];
+    // define undefined methods as noops to prevent errors
+    for (var i = 0; i < m.length; i++) {
+      if (!window.console[m[i]])
+        window.console[m[i]] = function () {
+        };
+    }
+  }
  })();
