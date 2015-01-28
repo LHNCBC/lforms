738ad7d546c44062cf8032d58558ded015ef2cf4
Ye Wang <wangye@mail.nih.gov>
2015-01-28 17:09:07 -0500

    add the missing autocompPhr module

--- app/lforms.js
+++ app/lforms.js
@@ -1,7 +1,10 @@
+'use strict';
+
 angular.module('lformsWidget', [
     'ngRoute',
     "mgcrea.ngStrap",
-    'ui.select2'
+    'ui.select2',
+    'autocompPhr'
 ])
 .directive('lformsPanelV', function() {
   return {
