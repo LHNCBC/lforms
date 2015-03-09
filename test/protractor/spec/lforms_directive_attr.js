13fd3d58f193606fdb4c4516402f175cdb398b9d
akanduru <akanduru@mail.nih.gov>
2015-03-09 16:03:28 -0400

    Add protractor tests for hidden check boxes of directive. Created a separate app/test folder, moved directieveTest.html and added directiveAttrTest.html

---
+++ test/protractor/spec/lforms_directive_attr.js
@@ -0,0 +1,57 @@
+
+var test_url = 'http://0.0.0.0:9001/test/directiveAttrTest.html';
+
+/**
+ * 
+ * @returns {WebElements} Return check boxes 
+ */
+function get_checkboxes() {
+  return element.all(by.css('div.checkbox > label > input[type="checkbox"]'));
+}
+
+/**
+ * 
+ * @param {WebElements} checkboxes The list of check boxes
+ * @param {integer} count Number of elements to assert.
+ * @param {boolean} hidden True asserts for hidden 
+ * @returns {Void}
+ */
+function assert_hidden_checkboxes(checkboxes, count, hidden) {
+  expect(checkboxes.count()).toEqual(count);
+  for(var i=0; i < count; i++) {
+    if(hidden) {
+      expect(checkboxes.get(i).isDisplayed()).toBeFalsy();
+    }
+    else {
+      expect(checkboxes.get(i).isDisplayed()).toBeTruthy();
+    }
+  }
+}
+
+describe('<lforms-panel-x> hide-check-boxes attribute', function() {
+  it('default should show check boxes on the vertical panel ', function() {
+    browser.get(test_url);
+    assert_hidden_checkboxes(get_checkboxes(), 2, false);
+  });
+  it('default should show check boxes on the horizontal panel ', function() {
+    browser.get(test_url+'#'+'/horizontal-form-default');
+    assert_hidden_checkboxes(get_checkboxes(), 2, false);
+  });
+  it('true should hide check boxes on the vertical panel', function() {
+    browser.get(test_url+'#'+'/vertical-form-hide-check-boxes-true');
+    assert_hidden_checkboxes(get_checkboxes(), 2, true);
+  });
+  it('true should hide check boxes on the horizontal panel', function() {
+    browser.get(test_url+'#'+'/horizontal-form-hide-check-boxes-true');
+    assert_hidden_checkboxes(get_checkboxes(), 2, true);
+  });
+  it('false should show check boxes on the vertical panel', function() {
+    browser.get(test_url+'#'+'/vertical-form-hide-check-boxes-false');
+    assert_hidden_checkboxes(get_checkboxes(), 2, false);
+  });
+  it('false should show check boxes on the horizontal panel', function() {
+    browser.get(test_url+'#'+'/horizontal-form-hide-check-boxes-false');
+    assert_hidden_checkboxes(get_checkboxes(), 2, false);
+  });
+});
+
