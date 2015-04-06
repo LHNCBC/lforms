a811b9a68527fecf9b3f32a859dcba6a7194aced
akanduru <akanduru@mail.nih.gov>
2015-04-06 11:57:43 -0400

    Add constants module for lforms

---
+++ app/scripts/lforms-constants.js
@@ -0,0 +1,8 @@
+/*
+ * Define lforms constants here and use this a dependency in the angular application
+ */
+
+angular.module('lfConstants',[])
+  .constant('LF_CONSTANTS', {
+    BLANK_GIF_DATAURL: 'data:image/gif;base64,R0lGODlhAQABAJEAAAAAAP///////wAAACH5BAUUAAIALAAAAAABAAEAAAICVAEAOw=='
+  });
