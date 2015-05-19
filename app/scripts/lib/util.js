e1e99cbb8f32649c484725614dd769d26a9c81bf
Ye Wang <wangye@mail.nih.gov>
2015-05-19 16:11:28 -0400

    undo a previous change that caused dataType 'CE' not converted to 'CNE'

--- app/scripts/lib/util.js
+++ app/scripts/lib/util.js
@@ -206,7 +206,8 @@ WidgetUtil = {
         itemList[i].header = false;
       }
       // dataType should not be null for questions have answers
-      if (!itemList[i].dataType && itemList[i].answers && itemList[i].answers.length > 0 ) {
+      // dateType might be 'CE' in data files from RI that have answers
+      if ( itemList[i].answers && itemList[i].answers.length > 0 ) {
         itemList[i].dataType = 'CNE';
       }
 
