0aa0d2d9235a59186d3f48a825637b3e28295a4d
Ye Wang <wangye@mail.nih.gov>
2015-05-21 14:17:05 -0400

    pass all the tests

--- test/protractor/spec/lforms_formula.js
+++ test/protractor/spec/lforms_formula.js
@@ -16,22 +16,23 @@ describe('formula', function() {
     var height1= element(by.id('/54126-8/8302-2/1/1')),
         weight1 = element(by.id('/54126-8/29463-7/1/1')),
         bmi1 = element(by.id('/54126-8/39156-5/1/1')),
-        heightUnit1 = element(by.id('ac15')),
-        weightUnit1 = element(by.id('ac16')),
+        heightUnit1 = element(by.id('ac1')),
+        weightUnit1 = element(by.id('ac2')),
         height2 = element(by.id('/54114-4/54117-7/8302-2/1/1/1')),
         weight2 = element(by.id('/54114-4/54117-7/29463-7/1/1/1')),
         bmi2 = element(by.id('/54114-4/54117-7/39156-5/1/1/1'));
 
-    browser.waitForAngular();
-
     // check bmi1
     height1.sendKeys("70");
-    expect(bmi1.getVal().toBe(""));
+    browser.waitForAngular();
+    expect(bmi1.getAttribute('value')).toBe("");
     weight1.sendKeys("170");
-    expect(bmi1.getVal().toBe("24.39"));
+    browser.waitForAngular();
+    expect(bmi1.getAttribute('value')).toBe("24.39");
     height1.clear();
     height1.sendKeys("80");
-    expect(bmi1.getVal().toBe("18.68"));
+    browser.waitForAngular();
+    expect(bmi1.getAttribute('value')).toBe("18.68");
 
     // change height unit and check bmi1 again
     heightUnit1.click();
@@ -43,7 +44,7 @@ describe('formula', function() {
     browser.waitForAngular();
     height1.clear();
     height1.sendKeys("170");
-    expect(bmi1.getVal().toBe("16.68"));
+    expect(bmi1.getAttribute('value')).toBe("26.68");
 
     // change weight unit and check bmi1 again
     weightUnit1.click();
@@ -54,15 +55,15 @@ describe('formula', function() {
     weightUnit1.sendKeys(protractor.Key.TAB);
     weight1.clear();
     weight1.sendKeys("80");
-    expect(bmi1.getVal().toBe("17.68"));
+    expect(bmi1.getAttribute('value')).toBe("27.68");
 
     // check bmi2
     height2.sendKeys("70");
-    expect(bmi2.getVal().toBe(""));
+    expect(bmi2.getAttribute('value')).toBe("");
     weight2.sendKeys("170");
-    expect(bmi2.getVal().toBe("24.39"));
+    expect(bmi2.getAttribute('value')).toBe("24.39");
     height2.clear();
     height2.sendKeys("80");
-    expect(bmi2.getVal().toBe("18.68"));
+    expect(bmi2.getAttribute('value')).toBe("18.68");
   });
 });
