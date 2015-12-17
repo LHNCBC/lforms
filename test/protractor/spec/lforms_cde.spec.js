b55f4440c4027e0216d88249abcd04c8f0602eb2
Ye Wang <wangye@mail.nih.gov>
2015-12-17 11:51:22 -0500

    not to show links for codes that are not LOINC

--- test/protractor/spec/lforms_cde.spec.js
+++ test/protractor/spec/lforms_cde.spec.js
@@ -13,4 +13,17 @@ describe('CDE form template', function() {
     expect(headerLabel.getText()).toBe("");
   });
 
+
+  it('question code has no links if it is set to be shown', function() {
+
+    tp.openBaseTestPage();
+    tp.openFormByIndex(9);
+
+    var titleCode = element(by.css(".lf-form-title .item-code span"));
+    expect(titleCode.getText()).toBe("[5603071e1c7581941db4dd50]");
+    var titleCodeLink = element(by.css(".lf-form-title .item-code a"));
+    expect(titleCodeLink.isPresent()).toBe(false);
+
+  });
 });
+
