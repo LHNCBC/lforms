0fb26f6bc732a37e0136627057fc0b29caabbeee
Paul Lynch <plynch@mail.nih.gov>
2016-03-02 14:54:40 -0500

    Removed test that checked for loading of img tag images, because we do not have any that are not blank

--- test/protractor/spec/buildTest.spec.js
+++ test/protractor/spec/buildTest.spec.js
@@ -8,26 +8,4 @@ describe('build test page', function() {
       return po.searchResults.isDisplayed();
     }, 5000);
   });
-
-
-  fit('should have loaded image resources', function() {
-    // Borrowing code from http://stackoverflow.com/a/28343848/360782
-    // Unfortunately this code only checks the loading of image tags, not
-    // background images.  I'm not sure how to check those have loaded.
-    // Also, the only img tags we have at present are blank with a background
-    // image so this test is quite pointless.
-    po.openPage();
-    browser.executeAsyncScript(function (callback) {
-      var imgs = document.getElementsByTagName('img'),
-          loaded = 0;
-      for (var i = 0; i < imgs.length; i++) {
-        if (imgs[i].naturalWidth > 0) {
-          loaded = loaded + 1;
-        }
-      };
-      callback(imgs.length - loaded);
-    }).then(function (brokenImagesCount) {
-      expect(brokenImagesCount).toBe(0);
-    });
-  });
 });
