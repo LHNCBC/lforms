c9a6ce2bfdd45c031c10de0dd31e1f7b85cce8a0
Ye Wang <wangye@mail.nih.gov>
2016-03-14 13:49:38 -0400

    update form_definition.md

--- app/scripts/lib/lforms-data.js
+++ app/scripts/lib/lforms-data.js
@@ -90,7 +90,7 @@ var LFormsData = Class.extend({
   // default template options
   _defaultTemplateOptions: {
     showQuestionCode: false,   // whether question code is displayed next to the question
-    showCodingInstruction: false, // whether to show coding instruction inline. (false: inline; true: in a popup)
+    showCodingInstruction: false, // whether to show coding instruction inline. (false: in popover; true: inline)
     tabOnInputFieldsOnly: false, // whether to control TAB keys to stop on the input fields only (not buttons, or even units fields).
     hideHeader: false, // whether to hide the header section on top of the form
     hideCheckBoxes: false, // whether to hide checkboxes in the header section on top of the form
