5f76c45682efc62e852bc267e7ee3de9977bc98c
Ye Wang <wangye@mail.nih.gov>
2016-03-21 17:05:02 -0400

    fix a bug that hitting ENTER submits the form and triggers the ng-click event on buttons.

--- test/protractor/spec/lforms_datatype.spec.js
+++ test/protractor/spec/lforms_datatype.spec.js
@@ -11,3 +11,18 @@ describe('Data Type', function() {
 
 });
 
+describe('Button Type', function() {
+
+  it('Each button should have a type="button" so that ENTER does not submit a form (or trigger ng-click on the buttons)', function () {
+    tp.openUSSGFHTHorizontal();
+
+    var name1 = element(by.id('/54126-8/54125-0/1/1')),
+        name2 = element(by.id('/54126-8/54125-0/1/2'));
+
+    name1.click();
+    name1.sendKeys(protractor.Key.ENTER);
+    // nothing should happen
+    expect(name2.isPresent()).toBe(false);
+  });
+
+});
