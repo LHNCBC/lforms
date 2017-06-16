2ec9a7673c599df5cf3fde45fc650eb12148a202
Paul Lynch <plynch@mail.nih.gov>
2017-06-16 13:58:42 -0400

    Changed the HL7 v2 output to emit a separate OBX for each answer to multi-answer questions

--- test/mocha/hl7.spec.js
+++ test/mocha/hl7.spec.js
@@ -1,8 +1,9 @@
 // Tests for the HL7 generation library
 describe('HL7 library', function() {
-  describe('itemToField', function() {
+  "use strict";
+  describe('_itemToField', function() {
     it('should use LOINC as the default code system', function() {
-      formInfo = {obxIndex: 1};
+      var formInfo = {obxIndex: 1};
       var itemData = {'question': 'Country', 'questionCode': 'X1234-5',
         'value': {'text': 'United States', 'code': 'USA'}, 'dataType': 'CWE',
         '_obx4': ''};
@@ -14,7 +15,7 @@ describe('HL7 library', function() {
 
 
     it('should use answerCodeSystem when provided', function() {
-      formInfo = {obxIndex: 1};
+      var formInfo = {obxIndex: 1};
       var itemData = {'question': 'Country', 'questionCode': 'X1234-5',
         'answerCodeSystem': 'AA',
         'value': {'text': 'United States', 'code': 'USA'}, 'dataType': 'CWE',
@@ -27,7 +28,7 @@ describe('HL7 library', function() {
 
 
     it('should put uncoded CWE answers into OBX5.9', function() {
-      formInfo = {obxIndex: 1};
+      var formInfo = {obxIndex: 1};
       var itemData = {'question': 'Country', 'questionCode': 'X1234-5',
         'answerCodeSystem': 'AA',
         'value': {'text': 'United States'}, 'dataType': 'CWE',
@@ -37,7 +38,7 @@ describe('HL7 library', function() {
           'OBX|1|CWE|'+itemData.questionCode+'^'+itemData.question+
           '^LN||^^^^^^^^'+itemData.value.text+'|');
       // Try a multi-answer field
-      formInfo = {obxIndex: 1};
+      var formInfo = {obxIndex: 1};
       var itemData = {'question': 'Country', 'questionCode': 'X1234-5',
         'answerCodeSystem': 'AA',
         'value': [{'text': 'Canada'}, {'text': 'United States', 'code': 'USA'}],
@@ -45,7 +46,27 @@ describe('HL7 library', function() {
       var out = LForms.HL7._itemToField(itemData, formInfo);
       assert.equal(out.trim(),
           'OBX|1|CWE|'+itemData.questionCode+'^'+itemData.question+
-          '^LN||^^^^^^^^Canada~USA^United States^AA|');
+          "^LN|a|^^^^^^^^Canada|\r"+
+          'OBX|1|CWE|'+itemData.questionCode+'^'+itemData.question+
+          '^LN|b|USA^United States^AA|');
+    });
+
+
+    it('should put multiple answers into separate OBX records', function() {
+      // This is nearly the same test as above, but conceptually the tests
+      // should be distinct.
+      var formInfo = {obxIndex: 1};
+      var itemData = {'question': 'Country', 'questionCode': 'X1234-5',
+        'answerCodeSystem': 'AA',
+        'value': [{'text': 'Canada', 'code': 'CA'}, {'text': 'United States', 'code': 'USA'}],
+        'dataType': 'CWE', '_obx4': '2'};
+      var out = LForms.HL7._itemToField(itemData, formInfo);
+      assert.equal(out.trim(),
+          'OBX|1|CWE|'+itemData.questionCode+'^'+itemData.question+
+          "^LN|2.a|CA^Canada^AA|\r"+
+          'OBX|1|CWE|'+itemData.questionCode+'^'+itemData.question+
+          "^LN|2.b|USA^United States^AA|");
+
     });
   });
 
