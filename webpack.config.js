1e036bdb288ee30ea9cb8ab486c7e7cbf14a35cd
Paul Lynch <plynch@mail.nih.gov>
2018-10-15 15:40:27 -0400

    Revised the webpack config to handle multiple FHIR versions

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,50 @@
-module.exports = [{
-  node: {
-    fs: "empty"
-  },
-  entry:{
-    fhirpath: './app/scripts/fhir/STU3/fhirRequire.js',
-  },
-
-  mode: 'none',
-  output: {
-    path: __dirname,
-    filename: './app/scripts/fhir/STU3/lformsFHIR.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        // exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [['@babel/preset-env',
-              {
-                "targets": {
-                  "browsers": "ie >= 10"
+function commonConfig() {
+  return {
+    node: {
+      fs: "empty"
+    },
+    output: {
+      path: __dirname,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          // exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [['@babel/preset-env',
+                {
+                  "targets": {
+                    "browsers": "ie >= 10"
+                  }
                 }
-              }
-            ]]
+              ]]
+            }
           }
         }
-      }
-    ]
+      ]
+    }
   }
-}, {
-  node: {
-    fs: "empty"
-  },
-  entry:{
-    fhirpath: './app/scripts/fhir/STU3/fhirRequire.js',
-  },
+}
 
-  mode: 'production',
-  output: {
-    path: __dirname,
-    filename: './dist/latest/fhir/STU3/lformsFHIR.min.js',
-  }
-}];
+let configs = [];
+let fhirVersions = Object.keys(require('./app/scripts/fhir/versions'));
+for (let version of fhirVersions) {
+  let entryFile = './app/scripts/fhir/'+version+'/fhirRequire.js';
+
+  let nonMinConfig = commonConfig();
+  nonMinConfig.entry = entryFile;
+  nonMinConfig.output.filename = './app/scripts/fhir/'+version+'/lformsFHIR.js';
+  nonMinConfig.mode = 'none';
+  configs.push(nonMinConfig);
+
+  let minConfig = commonConfig();
+  minConfig.entry = entryFile;
+  minConfig.output.filename = './dist/latest/fhir/'+version+'/lformsFHIR.min.js',
+  minConfig.mode = 'production';
+  configs.push(minConfig);
+}
+
+module.exports = configs;
