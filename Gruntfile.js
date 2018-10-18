4a3c0798e885de34cfcaca7bb03eb0eee5997d31
Paul Lynch <plynch@mail.nih.gov>
2018-10-17 20:53:03 -0400

    Removed grunt-google-cdn, which is not needed for this package because it is not itself a web application

--- Gruntfile.js
+++ Gruntfile.js
@@ -407,13 +407,6 @@ module.exports = function (grunt) {
       }
     },
 
-    // Replace Google CDN references
-    cdnify: {
-      dist: {
-        html: ['<%= yeoman.dist %>/*.html']
-      }
-    },
-
     // Copies remaining files to places other tasks can use
     copy: {
       dist: {
