c2d741263474757a50670c2334867f8faa068f36
Paul Lynch <plynch@mail.nih.gov>
2019-02-08 20:03:30 -0500

    Made the start of an entry point for an npm module for lforms

--- webpack.config.js
+++ webpack.config.js
@@ -47,12 +47,12 @@ for (let version of fhirVersions) {
   minConfig.mode = 'production';
   configs.push(minConfig);
 
-  // autocomplete-lhc
-  let acConfig = commonConfig();
-  acConfig.entry = './node_modules/autocomplete-lhc/source/index.js';
-  acConfig.output.filename = './app/generated/scripts/autocomplete-lhc.js';
-  acConfig.mode = 'none';
-  configs.push(acConfig);
+  // autocomplete-lhc and (eventually) other ES6 code
+  let es6Config = commonConfig();
+  es6Config.entry = './app/scripts/index.js';
+  es6Config.output.filename = './app/generated/scripts/autocomplete-lhc.js';
+  es6Config.mode = 'none';
+  configs.push(es6Config);
 }
 
 module.exports = configs;
