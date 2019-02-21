d5ae23fe065b857be344f2b5b2b507ce81dfe43e
Paul Lynch <plynch@mail.nih.gov>
2019-02-21 15:49:38 -0500

    Added test for loading data and unit using observationLinkPeriod (tests passing)

--- test/protractor/spec/R4/prepop.spec.js
+++ test/protractor/spec/R4/prepop.spec.js
@@ -28,14 +28,52 @@ describe('Form pre-population', function() {
                   callback({data: {fhirVersion: '3.0'}});
                 }
               }
+            },
+            search: function(queryParams) {
+              // For now, we just have one test.
+              return {
+                then: function(callback) {
+                  callback({
+                    "data": {
+                      "resourceType": "Bundle",
+                      "type": "searchset",
+                      "entry": [
+                        {
+                          "fullUrl": "https://launch.smarthealthit.org/v/r3/fhir/Observation/8919e529-7c56-41e0-a696-2b974d4c95ae",
+                          "resource": {
+                            "code": {
+                              "coding": [
+                                {
+                                  "system": "http://loinc.org",
+                                  "code": "29463-7",
+                                  "display": "Body Weight"
+                                }
+                              ],
+                              "text": "Body Weight"
+                            },
+                            "effectiveDateTime": "2016-06-29T19:14:57-04:00",
+                            "issued": "2016-06-29T19:14:57-04:00",
+                            "valueQuantity": {
+                              "value": 95,
+                              "unit": "kg",
+                              "system": "http://unitsofmeasure.org",
+                              "code": "kg"
+                            }
+                          }
+                        }
+                      ]
+                    }
+                  });
+                }
+              };
             }
           }
         }});
     });
-    tp.loadFromTestData('ussg-fhp.json', 'R4');
   });
 
   it('should be possible to pull in data from a FHIR context', function() {
+    tp.loadFromTestData('ussg-fhp.json', 'R4');
     expect(tp.USSGFHTVertical.name.getAttribute('value')).toBe("John Smith");
     expect(tp.USSGFHTVertical.dob.getAttribute('value')).toBe("12/10/1990");
     // expect(tp.USSGFHTVertical.gender.getAttribute('value')).toBe("Male"); // TBD
@@ -51,6 +89,14 @@ describe('Form pre-population', function() {
         0);
     }, fhirVersion);
     expect(launchContextExt).not.toBeNull();
+  });
+
+  it('should load values from observationLinkPeriod', function() {
+    tp.loadFromTestData('weightHeightQuestionnaire.json', 'R4');
+    var weightField = element(by.id('/29463-7/1'));
+    expect(weightField.getAttribute('value')).toBe('95');
+    var unitField = element(by.id('unit_/29463-7/1'));
+    expect(unitField.getAttribute('value')).toBe('kg');
   });
 });
 
