3942040d0efa0aeb145de01754386dc0bafc9d8a
Paul Lynch <plynch@mail.nih.gov>
2019-02-22 11:26:46 -0500

    webpack now building lforms files, but still using bower packages; mostly passing tests except for issues with angular 1.7

---
+++ app/scripts/bower-index.js
@@ -0,0 +1,21 @@
+//var angular = require('angular');
+var Def = require('autocomplete-lhc');
+var LForms = require('../lforms.js');
+LForms.Def = Def;
+require("./lforms-constants.js");
+require("./lforms-config.js");
+require("./lforms-controllers.js");
+require("./lforms-directives.js");
+require("./bootstrap-decorators.js");
+require("./lib/date.js");
+require("./lib/cross_browser.js");
+require("./lib/lforms-util.js");
+require("./lib/polyfill.js");
+require("./lib/lforms-hl7.js");
+require("./lib/lforms-validate.js");
+require("./lib/lforms-data.js");
+require("./lib/expression-processor.js");
+LForms.Util.FHIRSupport = require("./fhir/versions.js");
+require("../lforms.tpl.js");
+
+module.exports = LForms;
