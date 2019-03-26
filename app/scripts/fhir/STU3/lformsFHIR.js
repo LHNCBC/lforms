86ce0373cb9ca6353b23649bd76e9090881f9a34
Paul Lynch <plynch@mail.nih.gov>
2019-03-26 12:30:21 -0400

    Replaced npm-shrinkwrap.json with package-lock.json, and required angular 1.6.x

--- app/scripts/fhir/STU3/lformsFHIR.js
+++ app/scripts/fhir/STU3/lformsFHIR.js
@@ -18104,24 +18104,9 @@ engine.andOp = function (a, b) {
 };
 
 engine.xorOp = function (a, b) {
-  if (Array.isArray(b)) {
-    if (a === true) {
-      return true;
-    } else if (a === false) {
-      return [];
-    } else if (Array.isArray(a)) {
-      return [];
-    }
-  }
-
-  if (Array.isArray(a)) {
-    if (b === true) {
-      return true;
-    } else {
-      return [];
-    }
-  }
-
+  // If a or b are arrays, they must be the empty set.
+  // In that case, the result is always the empty set.
+  if (Array.isArray(a) || Array.isArray(b)) return [];
   return a && !b || !a && b;
 };
 
