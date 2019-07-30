f23852f6e258499bded0f997a774378bb064e69e
Paul Lynch <plynch@mail.nih.gov>
2019-07-30 17:10:47 -0400

    Changed the demo page so that the demos are only loaded when shown.

--- javascripts/demo.js
+++ javascripts/demo.js
@@ -1,26 +1,29 @@
 // Sets up hide/show links for the demos
-jQuery('.showSource').each(function(i, e) {
-  var jqE = jQuery(e);
-  jqE.after(
-    '<div style="display:none"> \
-       <iframe style="width: 100%; height: 400px" \
-        src="https://jsfiddle.net/'+jqE.attr("_src")+
-        '/embedded/result,js,html/dark/"> \
-       </iframe> \
-       <a class="hideSource">[Hide Demo]</a> \
-     </div>'
-  );
-});
-
-function showSource(e) {
-  e.target.style.display='none';
-  e.target.nextElementSibling.style.display='block';
+function showSource(event) {
+  var elem = event.target;
+  if (!elem._loadedDemo) {
+    elem._loadedDemo = true;
+    var jqE = jQuery(elem);
+    jqE.after(
+      '<div style="display:none"> \
+         <iframe style="width: 100%; height: 400px" \
+          src="https://jsfiddle.net/'+jqE.attr("_src")+
+          '/embedded/result,js,html/dark/"> \
+         </iframe> \
+         <a class="hideSource" onclick="hideSource(this)">[Hide Demo]</a> \
+       </div>'
+    );
+  }
+  elem.style.display='none';
+  elem.nextElementSibling.style.display='block';
 }
 jQuery('.showSource').click(showSource);
-jQuery('.hideSource').click(function(e) {
-  e.target.parentNode.style.display='none';
-  e.target.parentNode.previousElementSibling.style.display = '';
-});
+
+function hideSource(elem) {
+  elem.parentNode.style.display='none';
+  elem.parentNode.previousElementSibling.style.display = '';
+};
+
 // Show the first one
 jQuery('.showSource').first().click();
 
