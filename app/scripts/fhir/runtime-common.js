de0aedfcf9968237166b216dca8423b5ec1c522f
Paul Lynch <plynch@mail.nih.gov>
2020-08-31 11:19:59 -0400

    Added comments and made lfFieldName in processExtensions optional

--- app/scripts/fhir/runtime-common.js
+++ app/scripts/fhir/runtime-common.js
@@ -5,8 +5,18 @@ extProcessors[renderingStyle.extURL] = renderingStyle.processExtension;
 export function addCommonRuntimeFns(ns) {
   var self = ns;
 
+  /**
+   *  Processes the extensions on either lfNode, or lfNode[lfFieldName], if
+   *  lfFieldName is provided.  Only the extensions for which processors
+   *  are written (in the "extensions" sub-directory) are considered.
+   * @param lfNode the node in the LFormsData structure on which the information
+   *  from the extension will be stored.
+   * @param lfFieldName (optional).  Sometimes the extension information is on a
+   *  sub-node, (e.g. 'obj_text') in which case this should be the field for
+   *  retrieving that sub-node.
+   */
   self.processExtensions = function(lfNode, lfFieldName) {
-    var fieldData = lfNode[lfFieldName]; // could be item or _text
+    var fieldData = lfFieldName ? lfNode[lfFieldName] || lfNode;
     if (fieldData) {
       var extensions = fieldData.extension;
       if (extensions) {
