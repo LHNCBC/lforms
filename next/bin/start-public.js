1020d7dae550350d87b9af02ca44f27b4a733cbb
Wang, Ye (NIH/NLM/LHC) [E] <wangye@nih.gov>
2021-11-12 17:54:05 -0500

    update port to 4200

--- next/bin/start-public.js
+++ next/bin/start-public.js
@@ -1,6 +1,6 @@
 const hostName = require('os').hostname();
 const { spawn } = require('child_process');
-const ngArgs = ['serve', '--port', '8100', '--host', hostName];
+const ngArgs = ['serve', '--port', '4200', '--host', hostName];
 if (process.argv[2] && process.argv[2] === '--dist') {
   ngArgs.push('--configuration');
   ngArgs.push('production');
