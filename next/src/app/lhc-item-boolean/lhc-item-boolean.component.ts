e967971e1d47466b0450c912de747bbf8bda8700
xieh3 <hao.xie@nih.gov>
2021-11-18 16:25:45 -0500

    aria label for boolean component

--- next/src/app/lhc-item-boolean/lhc-item-boolean.component.ts
+++ next/src/app/lhc-item-boolean/lhc-item-boolean.component.ts
@@ -1,21 +1,28 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { LhcDataService} from '../../lib/lhc-data.service';
+import {AfterViewInit, Component, ElementRef, Input, OnInit} from '@angular/core';
+import {LhcDataService} from '../../lib/lhc-data.service';
 
 @Component({
   selector: 'lhc-item-boolean',
   templateUrl: './lhc-item-boolean.component.html',
   styleUrls: ['./lhc-item-boolean.component.css']
 })
-export class LhcItemBooleanComponent implements OnInit {
+export class LhcItemBooleanComponent implements OnInit, AfterViewInit {
   @Input() item: any;
 
-  constructor(public lhcDataService: LhcDataService) { }
+  constructor(public lhcDataService: LhcDataService, private elRef:ElementRef) { }
 
   ngOnInit(): void {
   }
 
   ngOnDestroy() : void {
     //console.log('lhc-item-boolean, ngOnDestroy')
+  }
+
+  ngAfterViewInit() {
+    // Set id attribute of the actual <button> element, so that it is associated with the
+    // label in corresponding question text component and the label will be announced.
+    const button = this.elRef.nativeElement.querySelector('button[nz-wave]');
+    button && (button.id = this.item._elementId);
   }
 
 }
