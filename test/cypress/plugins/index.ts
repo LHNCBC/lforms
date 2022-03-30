e2d82c14948b0302213ca417b066c9922df6d8b2
xieh3 <hao.xie@nih.gov>
2022-03-30 18:07:27 -0400

    recreate tmp/test

--- test/cypress/plugins/index.ts
+++ test/cypress/plugins/index.ts
@@ -1,8 +1,6 @@
 // Plugins enable you to tap into, modify, or extend the internal behavior of Cypress
 // For more info, visit https://on.cypress.io/plugins-api
 // module.exports = (on, config) => {}
-import * as tmp from 'tmp';
-import * as path from 'path';
 import * as fs from 'fs';
 
 module.exports = (on, config) => {
@@ -12,20 +10,15 @@ module.exports = (on, config) => {
       return null;
     },
     /**
-     * Creates a temporary file under [project root]/tmp.
+     * Creates a temporary file under [project root]/tmp/test.
      * Returns path relative to project root.
      */
     createTempFile({fileName, content}) {
-      tmp.setGracefulCleanup();
-      const tmpFolder = `${config.fileServerFolder}/tmp`;
-      if (!fs.existsSync(tmpFolder)){
-        fs.mkdirSync(tmpFolder);
-      }
-      const dirOptions = {prefix: 'lformsTest2', unsafeCleanup: true, tmpdir: tmpFolder};
-      const dirPath = tmp.dirSync(dirOptions).name;
-      const pathName = path.join(dirPath, fileName);
-      fs.writeFileSync(pathName, content);
-      return path.relative(config.fileServerFolder, pathName);
+      const tmpFolder = `${config.fileServerFolder}/tmp/test`;
+      fs.rmdirSync(tmpFolder, { recursive: true });
+      fs.mkdirSync(tmpFolder, { recursive: true });
+      fs.writeFileSync(`${tmpFolder}/${fileName}`, content);
+      return `tmp/test/${fileName}`;
     }
   });
 };
