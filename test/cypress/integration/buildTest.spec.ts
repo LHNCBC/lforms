d7629192d2d9bbbd533d91bf20f900168ba0067a
xieh3 <hao.xie@nih.gov>
2022-06-22 14:41:46 -0400

    remove protractor

--- test/cypress/integration/buildTest.spec.ts
+++ test/cypress/integration/buildTest.spec.ts
@@ -1,6 +1,7 @@
 import {TestPage} from '../support/lforms_testpage.po.js';
 import {BuildTestPage} from '../support/buildTest.po.js';
 
+// build_test.html uses a copy of the transpiled lhc-forms.es5.js in dist/webcomponent/lhc-forms.es5.js
 describe('build test page', () => {
   const tp: TestPage = new TestPage();
   const btp: BuildTestPage = new BuildTestPage();
