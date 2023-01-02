d9d291c6031c21f6c7bf849e839b04da49fab5d6
nikhil-ht-el <ngorde@elimu.io>
2023-01-02 18:42:47 +0530

    Added logic for dynamic headers

--- src/app/lhc-item-gird/lhc-item-gird.component.ts
+++ src/app/lhc-item-gird/lhc-item-gird.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { LhcDataService} from '../../lib/lhc-data.service';
+import * as _ from "lodash";
 
 @Component({
   selector: 'lhc-item-gird',
@@ -11,8 +12,22 @@ export class LhcItemGirdComponent  {
 
   @Input() item;
   @Input() formLevel: boolean = false;
-  
+  uniqueGridHeaders: any[] = [];
   constructor(public lhcDataService: LhcDataService) {
+  }
+
+  ngOnInit(): void {
+    this.createCustomHeaders(this.item);
+  }
+ 
+  createCustomHeaders(item): void{
+    _.forEach(item.items, (questionItems, key) => {
+      _.forEach(questionItems.items, (subItems,key) => {  
+        if(!_.includes(this.uniqueGridHeaders, subItems.question)){
+          this.uniqueGridHeaders.push(subItems.question);
+        }
+      });
+    });
   }
 
 }
