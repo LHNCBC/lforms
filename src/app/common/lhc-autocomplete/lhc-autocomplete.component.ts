a7c98813082d940c32213d9900ba3e0f6a1ba3dd
Wang, Ye (NIH/NLM/LHC) [E] <wangye@nih.gov>
2023-06-08 11:47:51 -0400

    update comments

--- src/app/common/lhc-autocomplete/lhc-autocomplete.component.ts
+++ src/app/common/lhc-autocomplete/lhc-autocomplete.component.ts
@@ -471,12 +471,13 @@ export class LhcAutocompleteComponent implements OnChanges {
    * Get the selected answer object from a 'search' autocompleter
    * @param itemText answer's text
    * @param onList whether the answer's text matches the answers texts on the list
+   * @returns {{}} an answer object where 'code_system' is renamed to 'system' if 
+   *               there is a 'code_system'
    */
+
   getSearchItemModelData(itemText, onList) {
-    var rtn;
-    if (itemText === '')
-      rtn = null;
-    else {
+    var rtn =null;
+    if (itemText !== '') {
       let item = this.acInstance.getItemData(itemText);
       if (onList) {
         rtn = item;
