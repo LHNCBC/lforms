e76de19cc988e2f6e8f6c1b133999cf1a1fdd541
nikhil-ht <nikhilg@heaptrace.com>
2023-08-09 13:29:57 +0530

    log the mismatched item on console if and only if the mismatchedItem array contains item

--- src/fhir/sdc-import-common.js
+++ src/fhir/sdc-import-common.js
@@ -885,9 +885,9 @@ function addCommonSDCImportFns(ns) {
    * @param qr a QuestionnaireResponse instance
    * @returns {{}} an updated LForms form definition, with answer data
    */
-  let mismatchItem = [];
 
   qrImport.mergeQuestionnaireResponseToLForms = function (formData, qr) {
+    let mismatchItem = [];
     let itemsInBoth = qr.item[0].item.filter((obj1) => {
       let matched = formData.items[0].items.some((obj2) => {
         return obj1.linkId === obj2.linkId;
@@ -900,8 +900,9 @@ function addCommonSDCImportFns(ns) {
       //Handle the missmatch between questionnaire and questionnaireResponse 
       //Keep only those item in qr whoes present in  formData
     });
-
-    console.log('Mismatched items linkId:', mismatchItem);
+    if (mismatchItem.length > 0) {
+      console.log('Mismatched items linkId:', mismatchItem);
+    }
     qr.item[0].item = itemsInBoth;
     if (!(formData instanceof LForms.LFormsData)) {
       // get the default settings in case they are missing in the form data
