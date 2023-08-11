de9b6d7b951f2e0d5f9fe4ce5749c64a0d23db6d
Paul Lynch <plynch@mail.nih.gov>
2023-08-11 17:18:54 -0400

    Changed the import of jQuery so that we never use the external jQuery (if there is one) and also updated autocomplete-lhc to 18.6.3

--- src/app/lforms.js
+++ src/app/lforms.js
@@ -2,7 +2,7 @@ import * as ucumPkg from '@lhncbc/ucum-lhc';
 import LhcFormUtils from '../lib/lforms/lhc-form-utils.js';
 import LhcFormData  from '../lib/lforms/lhc-form-data';
 import LhcHL7 from '../lib/lforms/lhc-hl7.js';
-import './importJQuery'; // sets window.jQuery if not set; also sets window.LForms
+import './importJQuery'; // sets window.jQuery, needed for autocomplete-lhc, if not set; also sets window.LForms
 import Def from 'autocomplete-lhc'; // see docs at http://lhncbc.github.io/autocomplete-lhc/docs.html
 import Validation from '../lib/lforms/lhc-form-validation.js';
 import * as FHIRSupport from '../fhir/versions.js';
@@ -17,9 +17,12 @@ LForms.Def = Def;
 LForms.Validations =  Validation;
 LForms.lformsVersion = version.lformsVersion
 //LForms.jQuery is set above in importJQuery
-
-if (LForms.setJQuery) {
-  delete window.jQuery;
-  delete LForms.setJQuery;
-}
 LForms.Util.FHIRSupport = FHIRSupport;
+
+// Above in ./importJQuery, we temporarily set window.jQuery, which is needed
+// for the autocomplete-lhc initialization.  Reset that now.
+if (LForms.externalJQuery)
+  window.jQuery = LForms.externalJQuery;
+else
+  delete window.jQuery;
+delete LForms.externalJQuery;
