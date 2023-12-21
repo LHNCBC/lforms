f79505e321c2ec9ab88400030c70a7c9d253b734
Paul Lynch <plynch@mail.nih.gov>
2023-12-21 09:48:23 -0500

    Removed tests about disabling expressions in child questions of unanswered parent questions, which is a change we might not make.

--- test/karma/q-with-child-q.js
+++ test/karma/q-with-child-q.js
@@ -5,7 +5,7 @@ describe('Unanswered question with an answered child question', ()=>{
     lfData = new LForms.LFormsData({fhirVersion: 'R4', items: [{
       linkId: 'parentQ', dataType: 'ST', items: [{
         linkId: 'childQ', dataType: 'ST', value: "green",
-        extension: [{
+        extension: [{  // this is in case we later disable expressions for such cases; not used yet
           "url": LForms.FHIR.R4.SDC.fhirExtCalculatedExp,
           "valueExpression": {
             "language": "text/fhirpath",
@@ -21,15 +21,6 @@ describe('Unanswered question with an answered child question', ()=>{
     assert.equal(qr.item, undefined);
   });
 
-// TBD - will uncomment and fix as a part of a separate task.
-//  it('should not run expressions for the child questions', ()=>{
-//    return lfData._expressionProcessor.runCalculations().then(() => {
-//      assert.equal(lfData.items[0].items[0].value, 'green');
-//      let qr = LForms.Util.getFormFHIRData('QuestionnaireResponse', 'R4', lfData);
-//      assert.equal(qr.item, undefined);
-//    });
-//  });
-
   // Confirm that if we answer the parent questions, then the child question is
   // handled.
   describe('After the parent question is answered', ()=>{
@@ -44,16 +35,5 @@ describe('Unanswered question with an answered child question', ()=>{
       assert.equal(qr.item[0].answer[0].valueString, 'two');
       assert.equal(qr.item[0].answer[0].item[0].answer[0].valueString, 'green');
     });
-
-
-// TBD - will uncomment and fix as a part of a separate task.
-//    it('should run expressions for the child questions', ()=>{
-//      return lfData._expressionProcessor.runCalculations().then(() => {
-//        let qr = LForms.Util.getFormFHIRData('QuestionnaireResponse', 'R4', lfData);
-//        assert.equal(qr.item[0].answer[0].item[0].answer[0].valueString, 'blue');
-//      });
-//    });
-
   });
-
 });
