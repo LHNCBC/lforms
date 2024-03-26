91955b802a3c59d1e38c18b8e57aca3599b388c1
Wang, Ye (NIH/NLM/LHC) [E] <wangye@nih.gov>
2024-03-25 22:42:11 -0400

    updated start-public

--- bin/start-public.js
+++ bin/start-public.js
@@ -1,12 +1,9 @@
 const hostName = require('os').hostname();
 const { spawn } = require('child_process');
-const ngArgs = ['serve', '--port', '4200',
-    '--host', '0.0.0.0', // binding all ips
-    '--public-host', hostName, // binding host name
-    '--configuration=debugging',
-    '--disable-host-check']; // Using a different host than the one passed to the "--host" flag might result in
-                             // websocket connection issues. You might need to use "--disable-host-check"
-                             // if that's the case.
+const ngArgs = ['serve', '--port=4200',
+    '--host=0.0.0.0', // binding all ips
+    '--public-host=http://localhost:4201', // for web socket client to be able to connect
+    '--configuration=debugging']
 
 spawn('ng', ngArgs, {
   stdio: 'inherit'
