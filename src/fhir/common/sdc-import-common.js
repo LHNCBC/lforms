eef853d284bc3c1c960aa392e8648b7919d655f4
xieh3 <hao.xie@nih.gov>
2025-02-19 11:00:45 -0500

    update copy extensions logic on extensionHandlers

--- src/fhir/common/sdc-import-common.js
+++ src/fhir/common/sdc-import-common.js
@@ -64,8 +64,8 @@ function addCommonSDCImportFns(ns) {
   ];
 
   // One way or the other, the following extensions are converted to lforms internal fields.
-  // Any extensions not listed here or that has an entry in extensionHandlers that returns
-  // true will be copied over to lforms as is.
+  // Any extensions not listed here will be copied over to lforms as is, unless it has an
+  // entry in extensionHandlers that does not return true (meaning specifically not to copy).
   self.handledExtensionSet = new Set([
     self.fhirExtUrlCardinalityMin,
     self.fhirExtUrlCardinalityMax,
@@ -78,8 +78,6 @@ function addCommonSDCImportFns(ns) {
     self.fhirExtUrlMinLength,
     self.fhirExtUrlRegex,
     self.fhirExtUrlAnswerRepeats,
-    self.fhirExtUrlExternallyDefined,
-    "http://hl7.org/fhir/StructureDefinition/questionnaire-externallydefined",
     self.argonautExtUrlExtensionScore,
     self.fhirExtUrlHidden,
     self.fhirExtTerminologyServer,
@@ -1604,7 +1602,10 @@ function addCommonSDCImportFns(ns) {
       for (var i=0; i < qItem.extension.length; i++) {
         var ext = qItem.extension[i];
         var extHandler = self.extensionHandlers[ext.url];
-        if ((extHandler && extHandler(ext, lfItem)) ||
+        // Extensions should be copied if they aren't handled, which means
+        // 1) there isn't an extension handler or there is one that returns true (the signal to copy),  and
+        // 2) they are not in the handledExtensions list.
+        if ((!extHandler || (extHandler && extHandler(ext, lfItem))) &&
             !self.handledExtensionSet.has(qItem.extension[i].url)) {
           extensions.push(qItem.extension[i]);
         }
