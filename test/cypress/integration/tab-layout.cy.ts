971d53bfcbea546da8c0bf42cf339cdfdfbc3cab
xieh3 <hao.xie@nih.gov>
2025-07-22 14:41:18 -0400

    tab-layout-nested-layout.json

--- test/cypress/integration/tab-layout.cy.ts
+++ test/cypress/integration/tab-layout.cy.ts
@@ -47,4 +47,21 @@ describe('Tab Layout', () => {
     cy.byId('Q1-true/1/1/1').should('not.exist');
     cy.byId('Q1-false/1/1/1').should('be.visible');
   });
+
+  it('should render tab layout on nested layouts', () => {
+    util.addFormToPage('tab-layout-nested-layout.json', null, {fhirVersion: 'R4'});
+    // Horizontal layout under Tab One.
+    cy.byId('/g3/g1m1/1/1/1/1').should('be.visible');
+    cy.byId('/g3/g1m2/1/1/1/1').should('be.visible');
+    cy.byId('/g3/g1m3/1/1/1/1').should('be.visible');
+    cy.byId('/g3/g1m4/1/1/1/1').should('be.visible');
+    cy.byId('q1/1/1/1/1/1').should('not.exist');
+    // Nested tabs layout under Tab Two.
+    cy.byId('nz-tabs-0-tab-1').click();
+    cy.byId('q1/1/1/1/1/1').should('be.visible');
+    cy.byId('q2/1/1/1/1/1').should('not.exist');
+    cy.byId('nz-tabs-1-tab-1').click();
+    cy.byId('q1/1/1/1/1/1').should('not.be.visible');
+    cy.byId('q2/1/1/1/1/1').should('be.visible');
+  });
 });
