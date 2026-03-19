41c74f7a89f73f3e703efe46520195b09eeb345f
xieh3 <hao.xie@nih.gov>
2026-03-19 15:04:09 -0400

    add test for loading back merged QR with answerExpression

--- test/cypress/integration/question-with-sub-items.cy.ts
+++ test/cypress/integration/question-with-sub-items.cy.ts
@@ -121,4 +121,72 @@ describe('Question with sub items', () => {
     cy.byId('parent-checkbox/1||Orange').find('input[type="checkbox"]').should('not.be.checked');
     cy.byId('label-checkbox-subgroup-o/1/checkbox-subgroup-o').should('not.exist');
   });
+
+  it('should load back merged QR, when checkbox options are set by answerExpression', () => {
+    util.addFormToPage('checkbox-answerExpression-with-child-items.json', null, {fhirVersion: 'R4'});
+    // Build the answer options list for the checkbox question.
+    cy.byId('fruit/1').type('Apple');
+    cy.byId('add-fruit/1').click();
+    cy.byId('fruit/2').type('Banana');
+    // Select Apple and Banana.
+    cy.byId('parent-checkbox/1||Apple').click();
+    cy.byId('parent-checkbox/1||Banana').click();
+    // Fill out sub items for Apple and Banana.
+    cy.byId('child-integer/1/checkbox-subgroup-Apple/1').type('11');
+    cy.byId('child-integer/1/checkbox-subgroup-Banana/1').type('22');
+    cy.window().then((win) => {
+      // Verify the exports.
+      const q = win.LForms.Util.getFormFHIRData("Questionnaire", "R4");
+      const qr = win.LForms.Util.getFormFHIRData("QuestionnaireResponse", "R4");
+      expect(q.item[1].item.length).to.equal(1);
+      expect(q.item[1].item[0]).to.deep.equal({
+        "type": "integer",
+        "linkId": "child-integer",
+        "text": "How many of this?"
+      });
+      expect(qr.item[1].answer.length).to.equal(2);
+      expect(qr.item[1].answer[0]).to.deep.equal({
+        "valueCoding": {
+          "display": "Apple"
+        },
+        "item": [
+          {
+            "answer": [
+              {
+                "valueInteger": 11
+              }
+            ],
+            "linkId": "child-integer",
+            "text": "How many of this?"
+          }
+        ]
+      });
+      expect(qr.item[1].answer[1]).to.deep.equal({
+        "valueCoding": {
+          "display": "Banana"
+        },
+        "item": [
+          {
+            "answer": [
+              {
+                "valueInteger": 22
+              }
+            ],
+            "linkId": "child-integer",
+            "text": "How many of this?"
+          }
+        ]
+      });
+      // Load back the merged QR.
+      const formDef = win.LForms.Util.convertFHIRQuestionnaireToLForms(q, 'R4');
+      const mergedFormData = win.LForms.Util.mergeFHIRDataIntoLForms(qr, formDef, 'R4');
+      win.document.getElementById('formContainer').innerHTML = null;
+      cy.byId('label-checkbox-subgroup-Apple/1/checkbox-subgroup-Apple').should('not.exist');
+      win.LForms.Util.addFormToPage(mergedFormData, 'formContainer');
+      cy.byId('label-checkbox-subgroup-Apple/1/1').should('be.visible');
+      cy.byId('label-checkbox-subgroup-Banana/1/1').should('be.visible');
+      cy.byId('item-checkbox-subgroup-Apple/1/1').byId('child-integer/1/1/1').should('have.value', 11);
+      cy.byId('item-checkbox-subgroup-Banana/1/1').byId('child-integer/1/1/1').should('have.value', 22);
+    });
+  });
 });
