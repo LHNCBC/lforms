6bb82b4a5f0a6534925d5654f7e84af97b4de77e
xieh3 <hao.xie@nih.gov>
2026-04-20 12:18:24 -0400

    display as repeating answers for dropdown multiselect with child items

--- src/app/lhc-item-choice-autocomplete/lhc-item-choice-autocomplete.component.ts
+++ src/app/lhc-item-choice-autocomplete/lhc-item-choice-autocomplete.component.ts
@@ -34,4 +34,33 @@ export class LhcItemChoiceAutocompleteComponent implements OnChanges {
     }
   }
 
+  addOrRemoveSubGroups(): void {
+    if (!this.item.items) {
+      return;
+    }
+    const validLinkIds = this.item.value.map((v) => {
+      return {
+        'linkId': this.lhcDataService.getLhcFormData().getLinkIdForCheckboxSubGroup(v),
+        'matched': false
+      };
+    });
+    for (let i=0; i<this.item.items.length; i++) {
+      if (!this.item.items[i].isSubGroupForCheckbox) {
+        continue;
+      }
+      if (validLinkIds.includes(this.item.items[i].linkId)) {
+        validLinkIds.find((v) => v.linkId === this.item.items[i].linkId).matched = true;
+      }
+      else {
+        this.item.items.splice(i, 1);
+      }
+    }
+    validLinkIds.forEach((v) => {
+      if (!v.matched) {
+        this.lhcDataService.getLhcFormData().addSubItemsForCheckbox(this.item, v);
+      }
+    });
+    console.log(this.item.items);
+  }
+
 }
